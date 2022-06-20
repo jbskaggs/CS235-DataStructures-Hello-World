3a2247f973e1495fcc5d1d684c0fc121eeb6b3af
Jonathan Skaggs <jbskaggs12@gmail.com>
2022-06-20 13:22:25 -0600

    Hello World Program

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,3 +1,25 @@
 //
 // Created by jskag on 6/20/2022.
 //
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+
+    if (argc < 2) {
+        cout << "Hello World" << endl;
+    }
+    else {
+        cout << "Hello " << argv[1] << ", welcome to the world of computer science!" << endl;
+    }
+    return 0;
+}
+
+
+
+
+
+
